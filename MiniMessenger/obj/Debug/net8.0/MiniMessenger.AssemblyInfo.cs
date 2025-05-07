df02e5b1e7c4245a54a931e2b8bdaafa4844ae88
Tanvir Anjum Labir <tanvir.labir@gmail.com>
2025-05-07 17:03:51 +0600

    added docker support

--- MiniMessenger/obj/Debug/net8.0/MiniMessenger.AssemblyInfo.cs
+++ MiniMessenger/obj/Debug/net8.0/MiniMessenger.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("bce3c724-8e4c-4aba-b9f2-7e82101ebc72")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniMessenger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6322e2872b5eb1abf95f7d97d482620b91a0684a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b833e00bdf87b5fca53c28c01f9da932533661fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniMessenger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniMessenger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
